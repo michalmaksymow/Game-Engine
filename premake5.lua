334c6650b9f7487811ff3ab5e1315ede1c1fe0d8
unknown <borysbrat@gmail.com>
2020-03-19 10:04:03 +0100

    Add premake files

---
+++ premake5.lua
@@ -0,0 +1,102 @@
+workspace "Crown"
+ architecture "x64"
+
+ configurations
+ {
+  "Debug",
+  "Release",
+  "Dist"
+ }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Crown"
+ location "Crown"
+ kind "SharedLib"
+ language "C++"
+
+ targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+ objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+ files
+ {
+  "%{prj.name}/src/**.h",
+  "%{prj.name}/src/**.cpp"
+ }
+
+ includedirs
+ {
+  "%{prj.name}/vendor/spdlog/include"
+ }
+
+ filter "system:windows"
+  cppdialect "C++17"
+  staticruntime "On"
+  systemversion "latest"
+
+ 
+
+  defines
+  {
+   "CR_PLATFORM_WINDOWS",
+   "CR_BUILD_DLL"
+  }
+
+ filter "configurations:Debug"
+  defines "CR_DEBUG"
+  symbols "On"
+
+ filter "configurations:Release"
+  defines "CR_RELEASE"
+  optimize "On"
+
+ filter "configurations:Dist"
+  defines "CR_DIST"
+  optimize "On"
+
+project "Sandbox"
+ location "Sandbox"
+ kind "ConsoleApp"
+ language "C++"
+
+ targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+ objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+ files
+ {
+  "%{prj.name}/src/**.h",
+  "%{prj.name}/src/**.cpp"
+ }
+
+ includedirs
+ {
+  "Crown/vendor/spdlog/include",
+  "Crown/src"
+ }
+
+ links
+ {
+  "Crown"
+ }
+
+ filter "system:windows"
+  cppdialect "C++17"
+  staticruntime "On"
+  systemversion "latest"
+
+  defines
+  {
+   "CR_PLATFORM_WINDOWS"
+  }
+
+ filter "configurations:Debug"
+  defines "CR_DEBUG"
+  symbols "On"
+
+ filter "configurations:Release"
+  defines "CR_RELEASE"
+  optimize "On"
+
+ filter "configurations:Dist"
+  defines "CR_DIST"
+  optimize "On"
